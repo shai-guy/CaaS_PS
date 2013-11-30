68709f1aa9a7c6bf5d1f9d169a6f315506bae350
tin toy <tintoy@snoscillator.net>
2013-11-30 14:26:08 +1100

    Create initial XML-serialisable data contracts for CaaS Account, Role, and Status entities.

---
+++ ComputeClient/Compute.Contracts/Directory/Role.cs
@@ -0,0 +1,28 @@
+﻿using System.Xml.Serialization;
+
+namespace DD.CBU.Compute.Api.Contracts.Directory
+{
+	/// <summary>
+	///		Represents a role in the CaaS API.
+	/// </summary>
+	[XmlRoot("Role", Namespace = XmlNamespaceConstants.Directory)]
+	public class Role
+	{
+		/// <summary>
+		///		Create a new CaaS role data-contract.
+		/// </summary>
+		public Role()
+		{
+		}
+
+		/// <summary>
+		///		The name of the CaaS role.
+		/// </summary>
+		[XmlElement("name", Namespace = XmlNamespaceConstants.Directory)]
+		public string Name
+		{
+			get;
+			set;
+		}
+	}
+}
