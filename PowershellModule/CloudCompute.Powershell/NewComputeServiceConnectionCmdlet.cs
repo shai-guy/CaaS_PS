f4f553f8e1854c168e5808bf5042a2d3c4c01a99
tin toy <tintoy@snoscillator.net>
2013-12-08 11:23:17 +1100

    Create initial Powershell module.

---
+++ PowershellModule/CloudCompute.Powershell/NewComputeServiceConnectionCmdlet.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DD.CBU.Compute.Powershell
+{
+	/// <summary>
+	///		The "New-ComputeServiceConnection" Cmdlet.
+	/// </summary>
+	/// <remarks>
+	///		Used to create a new connection to the CaaS API.
+	/// </remarks>
+	[Cmdlet("New", "ComputeServiceConnection", SupportsShouldProcess = true)]
+	public class NewComputeServiceConnectionCmdlet
+		: Cmdlet
+	{
+		/// <summary>
+		///		Create a new <see cref="NewComputeServiceConnectionCmdlet"/>.
+		/// </summary>
+		public NewComputeServiceConnectionCmdlet()
+		{
+		}
+
+		/// <summary>
+		///		The credentials used to connect to the CaaS API.
+		/// </summary>
+		[Parameter(Mandatory = true, ValueFromPipeline = true)]
+		public PSCredential ApiCredentials
+		{
+			get;
+			set;
+		}
+	}
+}
