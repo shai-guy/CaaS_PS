6a567399214c641adee916910ab24488f88ca3ed
Shai Saar <avishay.saar@didata.com.au>
2014-09-16 10:41:37 +1000

    Added backup files for contract project

---
+++ ComputeClient/Compute.Contracts/Backup/BackupClientTypes.cs
@@ -0,0 +1,52 @@
+﻿namespace DD.CBU.Compute.Api.Contracts.Backup
+{
+    using System;
+    using System.Xml.Serialization;
+
+    /// <summary>
+    /// The backup client types
+    /// </summary>
+    [System.CodeDom.Compiler.GeneratedCode("xsd", "4.0.30319.18020")]
+    [Serializable]
+    [System.Diagnostics.DebuggerStepThrough]
+    [System.ComponentModel.DesignerCategory("code")]
+    [XmlType(AnonymousType = true, Namespace = XmlNamespaceConstants.Backup)]
+    [XmlRoot(Namespace = XmlNamespaceConstants.Backup, IsNullable = false)]
+    public class BackupClientTypes
+    {
+        /// <summary>
+        /// the items
+        /// </summary>
+        [XmlElement(ElementName = "backupClientType")]
+        public BackupClientType[] Items { get; set; }
+    }
+
+    /// <summary>
+    /// The backup client type
+    /// </summary>
+    [Serializable]
+    [System.Diagnostics.DebuggerStepThrough]
+    [System.ComponentModel.DesignerCategory("code")]
+    [XmlType(AnonymousType = true, Namespace = XmlNamespaceConstants.Backup)]
+    [XmlRoot(Namespace = XmlNamespaceConstants.Backup, IsNullable = false)]
+    public class BackupClientType
+    {
+        /// <summary>
+        /// the type
+        /// </summary>
+        [XmlAttribute]
+        public string type { get; set; }
+
+        /// <summary>
+        /// Is file system
+        /// </summary>
+        [XmlAttribute]
+        public string isFileSystem { get; set; }
+
+        /// <summary>
+        /// The description
+        /// </summary>
+        [XmlAttribute]
+        public string description { get; set; }
+    }
+}
